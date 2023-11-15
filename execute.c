81ce0150d21ca84bb28144cde85440d3ae4d4f45
WondwossenH9 <wondwossenhailu7@gmail.com>
2023-11-15 08:47:52 -0800

    Implement env builtin

--- execute.c
+++ execute.c
@@ -1,4 +1,7 @@
 #include "shell.h"
+static void execute_external_command(char *cmd_path, char **argv,
+		char *prog_name);
+static void handle_builtin_commands(char **argv, char *prog_name);
 
 /**
  * execute_command - Executes a command with arguments
@@ -7,11 +10,10 @@
  */
 void execute_command(char *cmd_line, char *prog_name)
 {
-	pid_t pid;
 	char *argv[MAX_ARGS];
 	char *token;
-	int status, i = 0;
 	char *cmd_path;
+	int i = 0;
 
 	token = strtok(cmd_line, " ");
 	while (token != NULL && i < MAX_ARGS - 1)
@@ -20,29 +22,63 @@ void execute_command(char *cmd_line, char *prog_name)
 		token = strtok(NULL, " ");
 	}
 	argv[i] = NULL;
-	if (strcmp(argv[0], "exit") == 0)
-		exit(EXIT_SUCCESS);
+	handle_builtin_commands(argv, prog_name);
 	cmd_path = find_command_in_path(argv[0]);
 	if (!cmd_path)
 	{
 		fprintf(stderr, "%s: %s: command not found\n", prog_name, argv[0]);
 		return;
 	}
+	execute_external_command(cmd_path, argv, prog_name);
+	free(cmd_path);
+}
+/**
+ * handle_builtin_commands - Checks and executes built-in commands
+ * @argv: Argument vector
+ * @prog_name: Program name for error messages
+ */
+static void handle_builtin_commands(char **argv, char *prog_name)
+{
+	if (strcmp(argv[0], "exit") == 0)
+	{
+		exit(EXIT_SUCCESS);
+	}
+	else if (strcmp(argv[0], "env") == 0)
+	{
+		for (int j = 0; environ[j] != NULL; j++)
+		{
+			printf("%s\n", environ[j]);
+		}
+		exit(EXIT_SUCCESS);
+	}
+}
+/**
+ * execute_external_command - Executes an external command
+ * @cmd_path: Full path to the command
+ * @argv: Argument vector for the command
+ * @prog_name: Program name for error messages
+ */
+static void execute_external_command(char *cmd_path, char **argv,
+		char *prog_name)
+{
+	pid_t pid;
+	int status;
+
 	pid = fork();
 	if (pid == 0)
 	{
-		if (execve(cmd_path, argv, NULL) == -1)
+		if (execve(cmd_path, argv, environ) == -1)
 		{
 			perror(prog_name);
-			free(cmd_path);
 			exit(EXIT_FAILURE);
 		}
 	}
 	else if (pid > 0)
 	{
 		wait(&status);
-		free(cmd_path);
 	}
 	else
+	{
 		perror(prog_name);
+	}
 }
--- shell.h
+++ shell.h
@@ -9,6 +9,8 @@
 
 #define MAX_CMD_LEN 1024
 #define MAX_ARGS 64
+
+extern char **environ;
 void execute_command(char *cmd_line, char *prog_name);
 int read_command(char *cmd, char *prog_name);
 void print_prompt(void);
