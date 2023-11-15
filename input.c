53584a076435c6c98fb873782066ee44418c1234
bisoul1984 <fikertetadesse1403@gmail.com>
2023-11-15 11:06:00 -0800

    updated

--- input.c
+++ input.c
@@ -2,24 +2,25 @@
 
 /**
  * read_command - Reads a command from stdin
- * @cmd: Buffer to store the command
- * @prog_name: Name of the program (unused)
- * Return: 1 on success, 0 on EOF, -1 on error
+ * @buffer: Buffer to store the command
+ * @size: Size of the buffer
+ * Return: Number of characters read
  */
-int read_command(char *cmd, char *prog_name)
+ssize_t read_command(char *buffer, size_t size)
 {
 	ssize_t len;
-	(void)prog_name;
 
-	len = read(STDIN_FILENO, cmd, MAX_CMD_LEN);
-	if (len <= 0)
-		return (len);
-	if (cmd[len - 1] == '\n')
-		cmd[len - 1] = '\0';
-	else
-		cmd[len] = '\0';
-	return (1);
+	len = read(STDIN_FILENO, buffer, size - 1);
+	if (len > 0)
+	{
+		if (buffer[len - 1] == '\n')
+			buffer[len - 1] = '\0'
+		else
+			buffer[len] = '\0';
+	}
+	return (len);
 }
+
 /**
  * print_prompt - Prints the shell prompt
  */
@@ -27,3 +28,4 @@ void print_prompt(void)
 {
 	write(STDOUT_FILENO, "$ ", 2);
 }
+
